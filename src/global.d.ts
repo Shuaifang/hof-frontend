54329ce3e2cf6f9534b6b231c457831c6ab26d81
minnzee <minnzee@syncswap.xyz>
2023-10-05 03:11:30 +0800

    update

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,3 @@
 // Declaring this interface provides type safety for message keys
-type Messages = typeof import('@/locale/messages/en-US.json');
+type Messages = typeof import('@/locale/messages/zh-CN.json');
 declare interface IntlMessages extends Messages {}
